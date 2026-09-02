d99b51ec8c7189f31db2c95971d3c2e299d798c1
Bitu kumar <rajbittu1114@gmail.com>
2026-09-02 16:33:03 +0530

    Time: 182 ms (37.25%), Space: 40.6 MB (58.08%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.py
@@ -0,0 +1,40 @@
+class Solution:
+    def sortList(self, head):
+        if head is None or head.next is None:
+            return head
+
+        slow = head
+        fast = head.next
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        mid = slow.next
+        slow.next = None
+
+        left = self.sortList(head)
+        right = self.sortList(mid)
+
+        return self.merge(left, right)
+
+    def merge(self, left, right):
+        dummy = ListNode(0)
+        temp = dummy
+
+        while left and right:
+            if left.val <= right.val:
+                temp.next = left
+                left = left.next
+            else:
+                temp.next = right
+                right = right.next
+
+            temp = temp.next
+
+        if left:
+            temp.next = left
+        else:
+            temp.next = right
+
+        return dummy.next
